13eb814e60392f7d3ee88c71f2288730a2a40bae
dinhvietvu153 <dinhvietvu15032004@gmail.com>
2024-10-08 08:52:43 +0700

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,1 +1,1 @@
-kubeadm tham gia 192.168.5.163:6443 --token irrgcf.ftvtmt7x07wp6901 --discovery-token-ca-cert-hash sha256:9f9728b2385228018e299c283b2737f8b31384df6ce91c41b9c98042b0f98b93
+sudo kubeadm join 192.168.5.163:6443 --token gh91fq.6jyrylydk6pouj99 --discovery-token-ca-cert-hash sha256:9f9728b23852280188299c283b2737f8b31384df6ce91c41b9c98042b0f98b93
