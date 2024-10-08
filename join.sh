b09f9511b22006b7f1fba779a4a98a0275f8d7ec
dinhvietvu153 <dinhvietvu15032004@gmail.com>
2024-10-08 08:40:19 +0700

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,3 +1,1 @@
-kubeadm tham gia 192.168.5.163:6443-token 7whrf3.qfpz4x40gy50n11h \
-
---discovery-token-ca-cert-hash sha256:803d17d775015bb4a42117319d876c8638cbf2972e09de0172233baa8ebf2a3e
+kubeadm tham gia 192.168.5.163:6443 --token irrgcf.ftvtmt7x07wp6901 --discovery-token-ca-cert-hash sha256:9f9728b2385228018e299c283b2737f8b31384df6ce91c41b9c98042b0f98b93
